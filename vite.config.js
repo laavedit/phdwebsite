9f5344c0e0b030eba8a5b0c982fccbf0ba4d39da
Av <166383858+laavedit@users.noreply.github.com>
2024-06-25 09:42:16 +0530

    Red

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/phdwebsite/', // This should match your GitHub Pages repository name
+  base: '/frontend/', // Ensure this matches your GitHub Pages repository name
   plugins: [react()],
 });
