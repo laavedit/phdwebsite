37e30b492bff57f7c8d34c029da0357eb57252fa
Av <166383858+laavedit@users.noreply.github.com>
2024-06-25 09:28:45 +0530

    Removed Workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/phdwebsite/', // Update this to match your GitHub Pages repository name
+  base: '/phdwebsite/', // This should match your GitHub Pages repository name
   plugins: [react()],
 });
