f9d62713958e2c81e82f58fd78d2a49a4f092979
Av <166383858+laavedit@users.noreply.github.com>
2024-06-25 15:07:40 +0530

    over and out for now

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: "/phdwebsite/", // Adjust base path as per your GitHub Pages setup
   plugins: [react()],
+  build: {
+    sourcemap: false,
   },
-);
+});
