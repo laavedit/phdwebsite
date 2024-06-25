824e52dac59dfc74b10d4f93f371014eec70224d
Av <166383858+laavedit@users.noreply.github.com>
2024-06-25 14:29:34 +0530

    hopefully final changes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/phdwebsite/', // Adjust base path as per your GitHub Pages setup
+  base: "/phdwebsite/", // Adjust base path as per your GitHub Pages setup
   plugins: [react()],
-  build: {
-    sourcemap: false, // Enable source maps in development (default)
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Clear any manualChunks setting
-      },
-    },
   },
-});
+);
