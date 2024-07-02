cd8d355797e29311c233fbfc0e927cf891b06f4e
Av <166383858+laavedit@users.noreply.github.com>
2024-07-02 11:30:52 +0530

    CSP Changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    headers: {
+      'Content-Security-Policy': `style-src 'nonce-random' 'self'`,
+    },
+  },
   base: "/phdwebsite/dist/", // Adjust base path as per your GitHub Pages setup
   plugins: [react()],
   build: {
