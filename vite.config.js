26080e1e85fd436ae2634748f4dcd35008db5f5f
Lalita Waldia <166383858+laavedit@users.noreply.github.com>
2024-07-02 11:01:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "https://peopleforhimalayas.in/phdwebsite/", // Adjust base path as per your GitHub Pages setup
+  base: "https://laavedit.github.io/phdwebsite/", // Adjust base path as per your GitHub Pages setup
   plugins: [react()],
   build: {
     sourcemap: false,
