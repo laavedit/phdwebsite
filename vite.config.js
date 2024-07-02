abbedcdab615c517caa9fe6777b6d07a06681722
Av <166383858+laavedit@users.noreply.github.com>
2024-07-02 11:21:40 +0530

    changing addidng /dist in vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/phdwebsite/", // Adjust base path as per your GitHub Pages setup
+  base: "/phdwebsite/dist/", // Adjust base path as per your GitHub Pages setup
   plugins: [react()],
   build: {
     sourcemap: false,
