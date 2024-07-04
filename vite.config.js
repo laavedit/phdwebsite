14624f19f2325b4bf2251b1d198b85dfa3037a76
Av <166383858+laavedit@users.noreply.github.com>
2024-07-04 11:29:00 +0530

    scr for assets

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/phdwebsite/", // Adjust base path as per your GitHub Pages setup
+  base: "/phdwebsite/", // Adjust this base path according to your deployment setup
   plugins: [react()],
   build: {
+    outDir: 'dist', // Output directory where Vite will generate the build
+    assetsDir: 'assets', // Directory for static assets like your auto-generated script
     sourcemap: false,
   },
 });
