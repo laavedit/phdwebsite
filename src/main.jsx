93c3349af0e0ff3af8c2d859c08de4c187268695
Av <166383858+laavedit@users.noreply.github.com>
2024-07-04 10:31:11 +0530

    CSP Changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './context/StoreContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/phdwebsite">
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
