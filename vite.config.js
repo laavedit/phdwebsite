50dc7927c01dae4e727bd301a8b46b07abf357a5
Av <166383858+laavedit@users.noreply.github.com>
2024-07-04 10:55:35 +0530

    wokring good locally

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    headers: {
-      'Content-Security-Policy': `style-src 'nonce-random' 'self'`,
-    },
-  },
-  base: "/phdwebsite/dist/", // Adjust base path as per your GitHub Pages setup
+  base: "/phdwebsite/", // Adjust base path as per your GitHub Pages setup
   plugins: [react()],
   build: {
     sourcemap: false,
